89736c65838a4b7e668675691c2a7be649fee83f
Pango02 <ludovicacerqua@gmail.com>
2023-08-20 23:34:02 +0200

    Swipe tra ingredienti palline
    Magari si può rivedere il codice per migliorarlo (ne parlerò col prof ), però per ora funziona!!
    
    ps: fa lo swipe solo con gli ingredienti accanto. Per fargli fare lo swipe bisogna selezionare le due palline

--- BubbleTea_Game/Assets/Scripts/MoveIngrediente.cs
+++ BubbleTea_Game/Assets/Scripts/MoveIngrediente.cs
@@ -1,74 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 
 public class MoveIngrediente : MonoBehaviour
 {
-    public int colonna;
-    public int riga;
-    public int TargetX;
-    public int TargetY;
-    private creazioneSfondo board;
-    private Vector2 firstTouchPosition;
-    private Vector2 finalTouchPosition;
-    public float swipeAngle = 0;
-    void Start ()
-    {
-        board = FindObjectOfType<creazioneSfondo>();
-        TargetX = (int)transform.position.x;
-        TargetY = (int)transform.position.y;
-
-    }
+    private bool isSelected = false;
+    private Vector3 posizione_da_cambiare= Vector3.zero;
+    [SerializeField] private float speed = 7;
+    
     void Update()
     {
-        TargetX = colonna;
-        TargetY = riga;
+        if (posizione_da_cambiare!= Vector3.zero)
+        {
+            this.transform.position= Vector3.Lerp(this.transform.position, posizione_da_cambiare, Time.deltaTime*speed);
+            if( this.transform.position== posizione_da_cambiare)
+            {
+                posizione_da_cambiare = Vector3.zero;
+            }
+        }
     }
     private void OnMouseDown()
     {
-        firstTouchPosition = Input.mousePosition;
-        //Debug.Log(firstTouchPosition);
+        isSelected = true;
+        Debug.Log(this.name + " è stato selezionato!");
     }
     private void OnMouseUp()
     {
-        finalTouchPosition = Input.mousePosition;
-        CalculateAngle();
+       
     }
-    void CalculateAngle() {
-        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
-        //Debug.Log(swipeAngle);
-        MovePieces();
-    }
-    void MovePieces()
-    {
-        if(swipeAngle > -45 && swipeAngle <= 45)
-        {
-            //swipe a destra
-            mat= board.mat[colonna + 1,riga];
-            mat.GetComponent<mat>().colonna -=1;
-            colonna += 1;
-        }
-        else if (swipeAngle > 45 && swipeAngle <= 135)
-        {
-            //swipe up
-            mat = board.mat[colonna , riga+1];
-            mat.GetComponent<mat>().riga -= 1;
-            riga += 1;
-        }
-        else if (swipeAngle > 135 && swipeAngle <= -135)
-        {
-            //swipe a sinistra
-            mat = board.mat[colonna - 1, riga];
-            mat.GetComponent<mat>().colonna += 1;
-            colonna -= 1;
-        }
-        else if (swipeAngle > -45 && swipeAngle <= -135)
-        {
-            //swipe down
-            mat = board.mat[colonna , riga-1];
-            mat.GetComponent<mat>().riga += 1;
-            riga -= 1;
-        }
-    }
+
+
+    
+
+    public void setPosizioneDaCambiare(Vector3 posizione_nuova) { posizione_da_cambiare = posizione_nuova; }
+    public void setIsSelected() { isSelected = false; }
+    public bool getIsSelected() { return isSelected; }
 }
--- BubbleTea_Game/Assets/Scripts/creazioneSfondo.cs
+++ BubbleTea_Game/Assets/Scripts/creazioneSfondo.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -11,6 +13,14 @@ public class creazioneSfondo : MonoBehaviour
     [SerializeField] public GameObject[] ingredienti_prefab;
     public GameObject[,] mat;
     public GameObject[,] mat_ingredienti;
+    private Vector3 vettore = new Vector3();
+    private GameObject ingredienti;
+    private int count = 1;
+
+    private GameObject ingrediente_da_scambiare;
+    private GameObject ingrediente_da_scambiare2;
+    private int i1, i2, j1, j2;
+  
 
 
     void Start()
@@ -21,10 +31,15 @@ public class creazioneSfondo : MonoBehaviour
             if (sfondo != null && ingredienti_prefab != null)
             {
                 mat_ingredienti = new GameObject[altezza, larghezza];
+                ingredienti = new GameObject("Ingredienti");
                 SetupMatrici();
             }
         }
 
+    }
+    private void Update()
+    {
+        ScambiaIngredienti();
     }
     private void SetupMatrici()
     {
@@ -32,7 +47,7 @@ public class creazioneSfondo : MonoBehaviour
         {
             for (int j = 0; j < larghezza; j++)
             {
-                Vector3 vettore = new Vector3();
+
                 Vector3 vettore_transform = this.transform.position;
                 Vector3 vettore_scala = this.transform.localScale;
                 Vector3 vettore_scala_ing = sfondo.transform.localScale;
@@ -44,10 +59,69 @@ public class creazioneSfondo : MonoBehaviour
                 mat[i, j].transform.parent = this.transform;
                 int num = Random.Range(0, ingredienti_prefab.Length);
                 vettore.z = vettore.z - 0.1f;
-                mat_ingredienti[i, j] = Instantiate(ingredienti_prefab[num], vettore, Quaternion.identity);
+                mat_ingredienti[i,j] = Instantiate(ingredienti_prefab[num], vettore, Quaternion.identity);
+                mat_ingredienti[i,j].name = "Ingrediente #" + count;
+                mat_ingredienti[i, j].transform.parent = ingredienti.transform;
+                count++;
             }
         }
 
+
+    }
+
+    public void ScambiaIngredienti()
+    {
+        ingrediente_da_scambiare = null;
+        ingrediente_da_scambiare2 = null;
+        for (int i = 0; i < mat_ingredienti.GetLength(0); i++)
+        {
+            for (int j = 0; j < mat_ingredienti.GetLength(1); j++)
+            {
+                if (mat_ingredienti[i, j].GetComponent<MoveIngrediente>().getIsSelected() && ingrediente_da_scambiare == null)
+                {
+                    ingrediente_da_scambiare = mat_ingredienti[i, j];
+                    i1 = i;
+                    j1 = j;
+                }
+                else if (mat_ingredienti[i, j].GetComponent<MoveIngrediente>().getIsSelected() && ingrediente_da_scambiare2 == null)
+                {
+                    ingrediente_da_scambiare2 = mat_ingredienti[i, j];
+                    i2 = i;
+                    j2 = j;
+                    break;
+                }
+            }
+
+        }
+
+
+        if (ingrediente_da_scambiare != null && ingrediente_da_scambiare2 != null)
+        {
+            ingrediente_da_scambiare.GetComponent<MoveIngrediente>().setIsSelected();
+            ingrediente_da_scambiare2.GetComponent<MoveIngrediente>().setIsSelected();
+
+            if (i1 == i2 - 1 || i1 == i2 + 1)
+            {
+                if (j1 == j2)
+                {
+                    ingrediente_da_scambiare.GetComponent<MoveIngrediente>().setPosizioneDaCambiare(ingrediente_da_scambiare2.transform.position);
+                    ingrediente_da_scambiare2.GetComponent<MoveIngrediente>().setPosizioneDaCambiare(ingrediente_da_scambiare.transform.position);
+                    mat_ingredienti[i1, j1] = ingrediente_da_scambiare2;
+                    mat_ingredienti[i2, j2] = ingrediente_da_scambiare;
+                }
+
+            }
+            else if (j1 == j2 - 1 || j1 == j2 + 1)
+            {
+                if (i1 == i2)
+                {
+                    ingrediente_da_scambiare.GetComponent<MoveIngrediente>().setPosizioneDaCambiare(ingrediente_da_scambiare2.transform.position);
+                    ingrediente_da_scambiare2.GetComponent<MoveIngrediente>().setPosizioneDaCambiare(ingrediente_da_scambiare.transform.position);
+                    mat_ingredienti[i1, j1] = ingrediente_da_scambiare2;
+                    mat_ingredienti[i2, j2] = ingrediente_da_scambiare;
+                }
+            }
+        }
     }
 
 }
