cd8b7ef45d70a53ded1675398c0b6d4880b679ec
jCutterwind <104735516+jCutterwind@users.noreply.github.com>
2023-08-23 13:47:51 +0200

    added an IngredientChecker script and redid the score system with 5 stars and average

--- BubbleTea_Game/Assets/Scripts/ScoreManager.cs
+++ BubbleTea_Game/Assets/Scripts/ScoreManager.cs
@@ -5,8 +5,81 @@ using UnityEngine;
 
 public enum starTier
 {
-    OneStar, TwoStar, ThreeStar
+    OneStar = 1, TwoStars = 2, ThreeStars = 3, FourStars = 4, FiveStars = 5
 }
+
+[Serializable] public class FixedFloatQueue
+{
+    [SerializeField] private int _size;
+    public int Size { get => this._size; set => this._size = value; }
+
+    [SerializeField] Queue<float> _queue = new Queue<float>();
+
+    public FixedFloatQueue(int size)
+    {
+        this._size = size;
+    }
+
+    public void Enqueue(float obj)
+    {
+        _queue.Enqueue(obj);
+        while(_queue.Count > _size)
+        {
+            _queue.Dequeue();
+        }
+    }
+
+    public float getAverage()
+    {
+        float result = 0.0f;
+        foreach(float obj in _queue)
+        {
+            result += obj;
+        }
+        if(result<= 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return result / _queue.Count;
+        }
+    }
+}
+
+
+[Serializable]
+public class ScoreMultiplier
+{
+    [SerializeField] private int scoreMult;
+    public int ScoreMult => scoreMult;
+    [SerializeField] private int maxScoreMult;
+    [SerializeField] private int maxScoreCounter;
+    [SerializeField] private int scoreMultCounter;
+
+    public void upMult()
+    {
+        scoreMultCounter++;
+
+        if (scoreMultCounter > maxScoreCounter)
+        {
+            if (scoreMult <= maxScoreMult)
+            {
+                scoreMult++;
+                scoreMultCounter = 0;
+            }
+        }
+    }
+
+    public void resetMult()
+    {
+        scoreMult = 0;
+    }
+}
+
+
+
+
 
 public class ScoreManager : MonoBehaviour
 {
@@ -14,110 +87,116 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private int downScore = 0;
     [SerializeField] private int maxIncrement = 5;
     [SerializeField] private ScoreMultiplier scoreMultiplier;
-    [SerializeField] private starTier currentTier = starTier.TwoStar;
-    [SerializeField] private int starCounter = 0;
-    [SerializeField] private int penaltyMult;
+    [SerializeField] private starTier currentTier;
+
+    [SerializeField] private int scoreMult = 0;
+    [SerializeField] private FixedFloatQueue oldStarsCounter;
+    [SerializeField] private int oldStarMaxNum = 5;
+    [SerializeField] private float currentStarNum = 3.3f;
+
+    [SerializeField] private float totalScore = 0;
 
 
-    [Serializable] public class ScoreMultiplier
+   
+
+    private void Start()
     {
-        [SerializeField] private int scoreMult;
-        public int ScoreMult => scoreMult;
-        [SerializeField] private int maxScoreMult;
-        [SerializeField] private int maxScoreCounter;
-        [SerializeField] private int scoreMultCounter;
-
-        public void upMult()
-        {
-            scoreMultCounter++;
-
-            if(scoreMultCounter>maxScoreCounter)
-            {
-                if(scoreMult<=maxScoreMult)
-                {
-                    scoreMult++;
-                    scoreMultCounter = 0;
-                }
-            }
-        }
-
-        public void resetMult()
-        {
-            scoreMult = 0;
-        }
-    }
+        oldStarsCounter = new FixedFloatQueue(oldStarMaxNum);
+        updateStars();
+    }   
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            ScoreUp(1);
-        }
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            ScoreDown(1);
+            updateStarsAverage(1);
         }
-
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            updateStarsAverage(2);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            updateStarsAverage(3);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            updateStarsAverage(4);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            updateStarsAverage(5);
+        }
     }
 
-    public void ScoreUp(int i)
+    public void updateDiff(float newPoints)
     {
-        updateTier(1);
-        scoreMultiplier.upMult();
-        if(downScore>0)
+        if(newPoints<=3)
         {
-            downScore -= 2*i;
-        }
-        if(downScore<=0)
-        {
-            downScore = 0;
-
-            this.upScore += i;
-            if (upScore > maxIncrement)
+            if (upScore > 0)
+            {
+                upScore -= 2;
+            }
+            if (upScore <= 0)
             {
                 upScore = 0;
-                GameManager.upDiff();
+
+                this.downScore++;
+                if (downScore > maxIncrement)
+                {
+                    downScore = 0;
+                    GameManager.downDiff();
+                }
             }
         }
-    }
-
-    public void ScoreDown(int i)
-    {
-        updateTier(-1);
-        scoreMultiplier.resetMult();
-        if(upScore>0)
+        else
         {
-            upScore -= 2*i;
-        }
-        if(upScore<=0)
-        {
-            upScore = 0;
-
-            this.downScore += i;
-            if (downScore > maxIncrement)
+            if (downScore > 0)
+            {
+                downScore -= 2;
+            }
+            if (downScore <= 0)
             {
                 downScore = 0;
-                GameManager.downDiff();
+
+                this.upScore++;
+                if (upScore > maxIncrement)
+                {
+                    upScore = 0;
+                    GameManager.upDiff();
+                }
             }
         }
     }
 
-    private void updateTier(int i)
+    private void updateStarsAverage(float newPoints)
     {
-        if (i < 0) i *= penaltyMult;
-        this.starCounter += i;
-        if(starCounter>maxIncrement)
+        this.oldStarsCounter.Enqueue(newPoints);
+        this.currentStarNum = this.oldStarsCounter.getAverage();
+        updateDiff(newPoints);
+        updateStars();
+        updateScore(newPoints);
+    }   
+
+    private void updateStars()
+    {
+        this.currentTier = (starTier)Mathf.FloorToInt(this.currentStarNum);
+    }
+
+    public void updateMult()
+    {
+        if (this.currentTier == starTier.FiveStars)
         {
-            starCounter = 0;
-            this.currentTier++;
-            if (currentTier > starTier.ThreeStar) this.currentTier = starTier.ThreeStar;
+            scoreMultiplier.upMult();
         }
-        if(starCounter<0)
+        else
         {
-            starCounter = maxIncrement;
-            this.currentTier--;
-            if (currentTier < starTier.OneStar) this.currentTier = starTier.OneStar;
+            scoreMultiplier.resetMult();
         }
     }
 
+    public void updateScore(float newPoints)
+    {
+        this.totalScore += newPoints * (int) currentTier;
+    }
 }
